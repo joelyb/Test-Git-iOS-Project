6bca0f521ef14381ec1c8d4e0847c20b990811ff
Joel Bensoor <joelbensoor@Joels-MacBook-Pro.local>
2014-08-10 19:01:32 +0100

    added a test class

---
+++ JBTest.h
@@ -0,0 +1,13 @@
+//
+//  JBTest.h
+//  Git Test
+//
+//  Created by Joel Bensoor on 10/08/2014.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JBTest : NSObject
+
+@end
---
+++ JBTest.m
@@ -0,0 +1,13 @@
+//
+//  JBTest.m
+//  Git Test
+//
+//  Created by Joel Bensoor on 10/08/2014.
+//
+//
+
+#import "JBTest.h"
+
+@implementation JBTest
+
+@end
